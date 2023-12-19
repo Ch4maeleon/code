20ba360f0eadc6d980365100f4fdeb363f1ce0fe
user <user@DESKTOP-RI6Q9AV>
2023-12-19 09:10:49 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsStudy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
